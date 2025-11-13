06cc920649ea10925b3a131907df22d49759b0e8
dannylit <itsdanlit@gmail.com>
2025-11-13 15:31:50 +0300

    add: new routes

--- server/src/routes/accounts.ts
+++ server/src/routes/accounts.ts
@@ -1,23 +1,41 @@
-import { requireApiKey, requireOwnership } from "@/internal/auth/middleware";
+import { requireOwnership, withAuth } from "@/internal/auth/middleware";
 import { createAccount, getAccountSettings, updateAccountSettings } from "@/internal/db/redis";
-import { AccountSettingsSchema, type AccountSettings } from "@/internal/db/schema";
+import { AccountSettingsSchema } from "@/internal/db/schema";
+import { tryCatchAsync } from "@/utils/try-catch";
 import Elysia, { t } from "elysia";
 
 export const accountRoutes = new Elysia({ prefix: "/accounts" })
-    .use(requireApiKey)
+    .use(withAuth)
     .post("/", async ({ apiKey, set }) => {
-        const accountId = createAccount(apiKey);
+        console.log(apiKey);
+        const [accountId, err] = await tryCatchAsync(createAccount(apiKey));
+
+        if (err) {
+            set.status = 401;
+            return { error: `failed to create account: ${err.message}` }
+        }
         set.status = 201;
         return { accountId };
     })
+    .group("/:id", (app) => app
+        .use(requireOwnership)
+        .get("/settings", async ({ params, set }) => {
+            const [settings, err] = await tryCatchAsync(getAccountSettings(params.id));
+            if (err) {
+                set.status = 404;
+                return { error: `failed to get settings: ${err.message}` }
+            }
 
-    .use(requireOwnership)
-    .get("/:id/settings", async ({ params }) => {
-        return await getAccountSettings(params.id)
-    })
-    .post("/:id/settings", async ({ params, body }) => {
-        await updateAccountSettings(params.id, body)
-        return { success: true };
-    }, { body: AccountSettingsSchema })
+            set.status = 201;
+            return settings;
+        })
+        .post("/settings", async ({ params, body, set }) => {
+            const [_, err] = await tryCatchAsync(updateAccountSettings(params.id, body))
+            if (err) {
+                set.status = 404;
+                return { error: `update settings: ${err.message}` };
+            };
 
-
+            set.status = 201;
+        }, { body: AccountSettingsSchema })
+    );
