028f39b46197ef42119b902b09136a9082b66b07
dannylit <itsdanlit@gmail.com>
2025-11-23 00:39:58 +0300

    fixed resize position handler

--- server/src/img_processor/resolver/resizing/resize.ts
+++ server/src/img_processor/resolver/resizing/resize.ts
@@ -5,6 +5,7 @@ export const resolveResize = (ctx: TransformationResolver, value: ResizeParams):
     const { width, height } = value;
     if (!width && !height) throw new Error("resize requires at least one dimension");
     const { position, extract, aspectRatio, zoom } = ctx.getMods(["position", "extract", "aspectRatio", "zoom"]);
+    console.log(position);
 
     if (width && height) {
         if (extract) { // extr with pos
@@ -20,64 +21,38 @@ export const resolveResize = (ctx: TransformationResolver, value: ResizeParams):
         };
 
         if (position) {
-            // goal is to get 600x200 box from 1200x893 pixel image. 
-            // when i preform (600x200) resize i get an image - width constrained but the height get cut off. I feel like internally its 600x447.
-            // const { x, y } = position; // y-30 is the padding i wanna apply to the top. So the image will be 600x200 starting from the 30 pixel on the y axis.
-            // const origRatio = ctx.state.width / ctx.state.height;
-            // const currRatio = width / height;
-
-            // const boxWidth = (origRatio > currRatio) ? Math.round(height * origRatio) : width; 
-            // const boxHeight = (origRatio < currRatio) ? Math.round(width / origRatio) : height;
-
-            // const xMax = boxWidth - width;
-            // const yMax = boxHeight - height;
-
-            // if ((xMax - x) < 0) throw new Error("x out of boundary");
-            // if ((yMax - y) < 0) throw new Error("y out of boundary");
-
-            // ctx.addInstructionV2("resize", { width: boxWidth, height: boxHeight });
-            // ctx.addInstructionV2("extract", { left: x ?? 0, top: y ?? 0, width: width, height: height });
-
-            // ctx.state.applyResize({ width, height });
-            // return;
             const { x, y } = position;
             const { width: origW, height: origH } = ctx.state;
 
             const scaleX = origW / width;
             const scaleY = origH / height;
 
-            const scale = Math.min(scaleX, scaleY);
+            const scale = Math.min(scaleX, scaleY); // dimension scale factor
 
-            if (scale === scaleX && x) throw new Error("x out of boundary"); // 1.37 
-            if (scale === scaleY && y) throw new Error("y out of boundary"); // 1.6
+            if (scale === scaleX) {
+                if (x) throw new Error("x out of boundary"); // constrained by width, no x movment possible
+                const scaledHeight = height * scale;
+                const scaledPadding = Math.round(y * scale);
+                const yAxisMaxPadding = Math.round(origH - scaledHeight);
+                if (scaledPadding > yAxisMaxPadding) throw new Error("y out of boundary");
+            };
+            if (scale === scaleY) {
+                if (y) throw new Error("y out of boundary"); // constrained by height, no y movement possible
+                const scaledWidth = width * scale;
+                const scaledPadding = Math.round(x * scale);
+                const xAxisMaxPadding = Math.round(origW - scaledWidth);
+                if (scaledPadding > xAxisMaxPadding) throw new Error("x out of boundary");
+            };
 
-            if (scale === scaleX && y > (origH / 2)) // e.g origW=893 y=(2.5*2
-
-                const left = (scale === scaleX) ? 0 : Math.round(scale * x);
+            const left = (scale === scaleX) ? 0 : Math.round(scale * x);
             const top = (scale === scaleY) ? 0 : Math.round(scale * y);
-
-            const sacledWidth = (scale === scaleY) ? Math.round(scale * width) : origW;
+            const scaledWidth = (scale === scaleY) ? Math.round(scale * width) : origW;
             const scaledHeight = (scale === scaleX) ? Math.round(scale * height) : origH;
+            ctx.addInstructionV2("extract", { left: left, top: top, width: scaledWidth, height: scaledHeight });
 
-            ctx.addInstructionV2("extract", { left: left, top: top, width: sacledWidth, height: scaledHeight });
             ctx.addInstructionV2("resize", { width, height });
+            ctx.state.applyResize({ width, height });
             return;
-
-            // if (targetRatio > origRatio) {
-            //     // constained by width
-            //     const scaleFactor = ctx.state.width / width;
-            //     if (x) throw new Error("x out of boundary");
-            //     ctx.addInstructionV2("extract", { left: 0, top: Math.round(scaleFactor * y), width: ctx.state.width, height: Math.round(scaleFactor * height) });
-            // }
-            // else if (targetRatio < origRatio) {
-            //     // constrained by height
-            //     const scaleFactor = ctx.state.height / height;
-            //     if (y) throw new Error("y out of boundary");
-            //     ctx.addInstructionV2("extract", { left: Math.round(scaleFactor * x), top: 0, width: Math.round(scaleFactor * width), height: ctx.state.height });
-            // };
-
-            // ctx.addInstructionV2("resize", { width, height });
-            // return;
         };
 
         ctx.addInstructionV2("resize", { width, height });
--- server/src/img_processor/resolver/resizing/zoom.ts
+++ server/src/img_processor/resolver/resizing/zoom.ts
@@ -5,15 +5,14 @@ export function resolveZoom(ctx: TransformationResolver, value: ZoomParams) {
     const { zoom } = value;
     if (!zoom) throw new Error("zoom value undefined");
 
-    const origWidth = ctx.state.width;
-    const origHeight = ctx.state.height;
+    const { width: origW, height: origH } = ctx.state;
 
-    const scaledWidth = Math.round(origWidth * zoom);
-    const scaledHeight = Math.round(origHeight * zoom);
+    const scaledWidth = Math.round(origW * zoom);
+    const scaledHeight = Math.round(origH * zoom);
 
-    const left = Math.round((scaledWidth - origWidth) / 2);
-    const top = Math.round((scaledHeight - origHeight) / 2);
+    const left = Math.round((scaledWidth - origW) / 2);
+    const top = Math.round((scaledHeight - origH) / 2);
 
     ctx.addInstructionV2("resize", { width: scaledWidth, height: scaledHeight });
-    ctx.addInstructionV2("extract", { left: left, top: top, width: origWidth, height: origHeight });
+    ctx.addInstructionV2("extract", { left: left, top: top, width: origW, height: origH });
 };
