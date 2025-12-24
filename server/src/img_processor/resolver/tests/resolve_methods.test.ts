880067f08d0e55848b86e74ab5f31311cfb7f4b2
dannylit <itsdanlit@gmail.com>
2025-12-24 19:00:48 +0300

    add unit tests

--- server/src/img_processor/resolver/tests/resolve_methods.test.ts
+++ server/src/img_processor/resolver/tests/resolve_methods.test.ts
@@ -3,22 +3,88 @@ import fs from "fs";
 import sharp from "sharp";
 import { ImageState } from "../image_state";
 import { TransformationResolver } from "@/img_processor/resolver/main";
+import path from "path";
 
-const testTransformations = JSON.parse(fs.readFileSync("./test-transformations.json", "utf-8"));
+async function getTestSuite() {
+    const testSuite: Record<string, any> = {};
+    const imageDirs = fs.readdirSync("./images");
+    for (const dirName of imageDirs) {
+        const dirPath = `./images/${dirName}`;
 
-describe("resolveResize", async () => {
-    const imagePaths = ["./audi.png"];
-    const imgBuffers = imagePaths.map(imgPath => fs.readFileSync(imgPath));
-    const imagesMeta = await Promise.all(imgBuffers.map(async (buf) => await sharp(buf).metadata()));
-    const images = imagesMeta.map(meta => new ImageState(meta));
+        const imgFile = fs.readdirSync(dirPath).find(f => f.startsWith("original."));
+        if (!imgFile) throw new Error(`original image not found in ${dirPath}`);
 
-    for (const { name, chain, expected } of testTransformations) {
-        test(name, () => {
-            for (const img of images) {
+        const trasformationFile = fs.readdirSync(dirPath).find(f => f === "resize_test.json");
+        if (!trasformationFile) throw new Error(`resize.json file not found in ${dirPath}`);
+
+        const transformations = JSON.parse(fs.readFileSync(path.join(dirPath, trasformationFile), "utf8"));
+        const buf = fs.readFileSync(path.join(dirPath, imgFile));
+        const meta = await sharp(buf).metadata();
+        testSuite[dirName] = { meta, transformations };
+    };
+
+    return testSuite;
+};
+
+const testSuite = await getTestSuite();
+
+describe("pure resize functions", () => {
+    for (const [imgName, suite] of Object.entries(testSuite)) {
+        const { meta, transformations } = suite;
+        for (const { name, input, expected } of transformations) {
+            test(`${imgName}:${meta.width}x${meta.height} -> ${name}`, () => {
+                const img = new ImageState(meta);
                 const resolver = new TransformationResolver(img);
-                const sharpIx = resolver.resolveChain(chain);
-                expect(sharpIx).toEqual(expected);
-            };
-        });
+
+                if ("error" in expected) {
+                    expect(() => resolver.resolveChain(input)).toThrow(expected.error);
+                    return;
+                };
+
+                const sharpIx = resolver.resolveChain(input);
+
+                expect(sharpIx.length).toBeGreaterThan(0);
+                expect(sharpIx[0]?.content.width).toBe(expected.content.width)
+                expect(sharpIx[0]?.content.height).toBe(expected.content.height)
+            });
+        };
     };
 });
+
+
+// test(`original:${img.origWidth}x${img.origHeight} -> ${name}`, () => {
+//     const resolver = new TransformationResolver(img);
+//     if ("error" in expected) {
+//         expect(() => resolver.resolveChain(chain)).toThrow(expected.error);
+//         return;
+//     };
+
+//     const sharpIx = resolver.resolveChain(chain);
+
+//     expect(sharpIx.length).toBeGreaterThan(0);
+//     expect(sharpIx[0]?.content.width).toBe(expected.content.width)
+//     expect(sharpIx[0]?.content.height).toBe(expected.content.height)
+//     expect(sharpIx[0]?.content.fit).toBeUndefined();
+//     expect(sharpIx[0]?.content.position).toBeUndefined();
+//     expect(sharpIx[0]?.content.background).toBeUndefined();
+// });
+// for (const { path, name, chain, expected } of testTransformations) {
+//     const img = imageStates[path];
+//     if (!img) throw new Error(`image not found: ${path}`);
+//     test(`original:${img.origWidth}x${img.origHeight} -> ${name}`, () => {
+//         const resolver = new TransformationResolver(img);
+//         if ("error" in expected) {
+//             expect(() => resolver.resolveChain(chain)).toThrow(expected.error);
+//             return;
+//         };
+
+//         const sharpIx = resolver.resolveChain(chain);
+
+//         expect(sharpIx.length).toBeGreaterThan(0);
+//         expect(sharpIx[0]?.content.width).toBe(expected.content.width)
+//         expect(sharpIx[0]?.content.height).toBe(expected.content.height)
+//         expect(sharpIx[0]?.content.fit).toBeUndefined();
+//         expect(sharpIx[0]?.content.position).toBeUndefined();
+//         expect(sharpIx[0]?.content.background).toBeUndefined();
+//     });
+// };
